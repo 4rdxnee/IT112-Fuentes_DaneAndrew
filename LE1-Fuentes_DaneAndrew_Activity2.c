3d54cdca0692f4ade6ff2a7c364418c274423be1
4rdxnee <fuentes.daneandrew@gmail.com>
2026-08-18 17:59:21 +0800

    Create LE1-Fuentes_DaneAndrew_Activity2.c

---
+++ LE1-Fuentes_DaneAndrew_Activity2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("************************************\n");
+    printf("*                                  *\n");
+    printf("*       WELCOME TO C PROGRAMMING   *\n");
+    printf("*                                  *\n");
+    printf("*       Learn - Code - Debug       *\n");
+    printf("*                                  *\n");
+    printf("*       FUTURE NETWORK ADMIN      *\n");
+    printf("*                                  *\n");
+    printf("************************************\n");
+
+    return 0;
+}
